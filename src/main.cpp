566f51befbf86c558c53fb3c1996c96cb9b39aac
sandy <sandlewjam@gmail.com>
2020-11-15 11:59:37 -0800

    Change resolution and app name

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "ivy/engine.h"
 
 int main() {
-    // Static so that destructor is called on fatal error exit()
     ivy::Engine engine;
+    engine.getOptions().appName = "Ivy Engine Demo";
+    engine.getOptions().renderWidth = 800;
+    engine.getOptions().renderHeight = 600;
+
     engine.run();
 
     return 0;
